507062ee74db03330035c3dc32f6498a43563952
basith670 <basithkdrmf87@gmail.com>
2026-07-11 10:49:21 +0530

    Build Jobs CRUD API using Django REST Framework

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import JobListCreateAPIView, JobDetailAPIView
 
 urlpatterns = [
-
+    path("", JobListCreateAPIView.as_view(), name="job-list-create"),
+    path("<int:pk>/", JobDetailAPIView.as_view(), name="job-detail"),
 ]
--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Job
+from .serializers import JobSerializer
+
+
+class JobListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [AllowAny]
+
+
+class JobDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [AllowAny]
